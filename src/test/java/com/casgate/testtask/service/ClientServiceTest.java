362acbff843e52accd354cb12eb5d98c9b9f4f22
spezdmtest <spezd.m.@gmail.com>
2024-01-04 16:34:14 +0200

    Unit Tests - ClientService

--- src/test/java/com/casgate/testtask/service/ClientServiceTest.java
+++ src/test/java/com/casgate/testtask/service/ClientServiceTest.java
@@ -24,7 +24,6 @@ public class ClientServiceTest {
 
     @Test
     public void findAll() {
-
         Mockito.when(clientRepository.findAll()).thenReturn(clientEntity);
 
         List<ClientEntity> clients = clientService.findAll();
